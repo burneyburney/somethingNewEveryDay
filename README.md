6e46c3c150ace4680ca6776b1bce390d1a52718a
michael burney <burney318@gmail.com>
2016-10-08 22:37:11 -0500

    updating

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 # Something New Every Day
+## change log:
 
 Mon - Sept 26, 7:55pm  
 learned 'let' in JavaScript
 
+Sat - Oct 8, 10:35 pm
+learned '' in JavaScript
--- letExample/let.js
+++ letExample/let.js
@@ -1,11 +1,9 @@
+"use strict";
 
+// because 'let' was used instead of 'var', it is only visiable for lines 13-15
+for(let i = 0; i < 5; i++){
+        console.log('inside of for loop, i = ' + i);
+};
 
-        "use strict";
-
-        // because 'let' was used instead of 'var', it is only visiable for lines 13-15
-        for(let i = 0; i < 5; i++){
-                console.log('inside of for loop, i = ' + i);
-        };
-
-        // which means line 18 has no clue what the variable 'i' represents
-        console.log('outside of for loop, i = ' + i);
+// which means line 18 has no clue what the variable 'i' represents
+console.log('outside of for loop, i = ' + i);
